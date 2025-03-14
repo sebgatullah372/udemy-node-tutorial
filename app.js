491ce6e6279ab5f63cbddc4e9178db3193d2717f
Md Sebgatullah Mahbub <smahbub@vantage.com>
2025-03-14 18:01:50 +0600

    returning response

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 const http = require('http');
-console.log('here ');
+
 const server = http.createServer((req, res) => {
     console.log('Request', req);
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<html>');
+    res.write('<head><title>Node js page</title></head>');
+    res.write('<body><h1>Writing from node server</h1></body>');
+    res.write('</html>');
 });
 
 server.listen(3000);

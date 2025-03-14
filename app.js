ac3e5d40de2d5ec600a6ff74ccea1f195786bb08
Md Sebgatullah Mahbub <smahbub@vantage.com>
2025-03-14 23:37:43 +0600

    Basic if else based routing and redirecting  and request body parsing using chunk and Buffer object

--- app.js
+++ app.js
@@ -1,7 +1,36 @@
 const http = require('http');
-
+const fs = require('fs');
 const server = http.createServer((req, res) => {
-    console.log('Request', req);
+    const url = req.url;
+    const method = req.method;
+
+    if(url === '/'){
+        res.write('<html>');
+        res.write('<head><title>Node js page</title></head>');
+        res.write('<body><form action="/message" method="POST"><input type="text" name="message"/><button type="submit">Send</button></form></body>');
+        res.write('</html>');
+        return res.end();
+    }
+
+    if(method === 'POST' && url === '/message'){
+        const body = [];
+        req.on('data', (chunk) => {
+            console.log('Chunk' , chunk);
+            body.push(chunk);
+        });
+        req.on('end', () => {
+            const parsedBody = Buffer.concat(body).toString();
+            console.log('ParsedBody', parsedBody);
+            const message = parsedBody.split('=')[1];
+            fs.writeFileSync('message.txt', message);
+            console.log('Written: ' + message);
+
+        });
+        res.statusCode = 302;
+        res.setHeader('Location', '/');
+        return res.end();
+    }
+
     res.setHeader('Content-Type', 'text/html');
     res.write('<html>');
     res.write('<head><title>Node js page</title></head>');

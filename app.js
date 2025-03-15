ee49518dbc5636e195733926208d6193485cf928
Md Sebgatullah Mahbub <smahbub@vantage.com>
2025-03-16 01:05:55 +0600

    basic routing and middleware

--- app.js
+++ app.js
@@ -3,6 +3,22 @@ const express = require('express');
 const app = express();
 
 app.use((req, res, next) => {
-    res.send('<h1>Express Page</h1>')
+    console.log('First Middleware');
+    next();
 });
+
+app.use((req, res, next) => {
+    console.log('Second Middleware');
+    next();
+});
+
+app.use('/users', (req, res, next) => {
+    res.send('<h1>Express Users Page</h1>')
+});
+
+app.use('/', (req, res, next) => {
+    res.send('<h1>Express Home Page</h1>')
+});
+
+
 app.listen(3000);

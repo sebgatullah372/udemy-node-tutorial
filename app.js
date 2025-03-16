5941261ec110749979b45dca5193a43ec01e2ec3
Md Sebgatullah Mahbub <smahbub@vantage.com>
2025-03-17 00:37:05 +0600

    added body parser to parse request body

--- app.js
+++ app.js
@@ -2,6 +2,12 @@ const express = require('express');
 
 const app = express();
 
+// Parse JSON request bodies
+app.use(express.json());
+
+// Parse URL-encoded request bodies (for form submissions)
+app.use(express.urlencoded({ extended: true }));
+
 app.use((req, res, next) => {
     console.log('First Middleware');
     next();
@@ -16,6 +22,13 @@ app.use('/users', (req, res, next) => {
     res.send('<h1>Express Users Page</h1>')
 });
 
+app.use('/add-product', (req, res, next)=> {
+    res.send('<form action="/product" method="POST"><input type="text" name="product"/><button type="submit">Add</button></form>');
+});
+app.use('/product', (req, res, next)=>{
+    console.log(req.body);
+    res.redirect('/');
+});
 app.use('/', (req, res, next) => {
     res.send('<h1>Express Home Page</h1>')
 });

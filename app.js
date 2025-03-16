3ced35ebbd7ce4c7d1b9fc60db0347a0173824ca
Md Sebgatullah Mahbub <smahbub@vantage.com>
2025-03-17 01:07:21 +0600

    added route prefix and 404 page

--- app.js
+++ app.js
@@ -10,7 +10,11 @@ app.use(express.json());
 // Parse URL-encoded request bodies (for form submissions)
 app.use(express.urlencoded({ extended: true }));
 
-app.use(adminRoutes);
+app.use('/admin', adminRoutes); // /admin is the route prefix
 app.use(shopRoutes);
+
+app.use((req, res, next)=>{
+    res.status(404).send('<h1>404 Page not found</h1>');
+})
 
 app.listen(3000);

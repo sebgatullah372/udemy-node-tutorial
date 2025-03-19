ab0e2a8f0cf96e80dbc4a61b17829d745db9a926
Md Sebgatullah Mahbub <smahbub@vantage.com>
2025-03-20 00:22:15 +0600

    serving html files

--- routes/admin.js
+++ routes/admin.js
@@ -1,5 +1,5 @@
 const express = require('express');
-
+const path = require('path');
 const router = express.Router();
 
 router.use('/users', (req, res, next) => {
@@ -7,7 +7,8 @@ router.use('/users', (req, res, next) => {
 });
 
 router.use('/add-product', (req, res, next)=> {
-    res.send('<form action="/product" method="POST"><input type="text" name="product"/><button type="submit">Add</button></form>');
+    // res.send('<form action="/product" method="POST"><input type="text" name="product"/><button type="submit">Add</button></form>');
+    res.sendFile(path.join(__dirname, '../', 'views', 'add-product.html'));
 });
 router.use('/product', (req, res, next)=>{
     console.log(req.body);
--- routes/shop.js
+++ routes/shop.js
@@ -1,10 +1,11 @@
 const express = require('express');
+const path = require('path');
 
 const router = express.Router();
 
 //Make sure exact match with / [GET]
 router.get('/', (req, res, next) => {
-    res.send('<h1>Express Home Page</h1>')
+    res.sendFile(path.join(__dirname, '../', 'views', 'shop.html'));
 });
 
 module.exports = router;

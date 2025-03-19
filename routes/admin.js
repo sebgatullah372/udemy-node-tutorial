e07924af32ffcaf36a5347f1a85c65c4e139b072
Md Sebgatullah Mahbub <smahbub@vantage.com>
2025-03-20 00:45:49 +0600

    constructing common root directory variable for using in the application

--- routes/admin.js
+++ routes/admin.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const path = require('path');
+const rootDir = require('../utils/path');
 const router = express.Router();
 
 router.use('/users', (req, res, next) => {
@@ -8,7 +9,7 @@ router.use('/users', (req, res, next) => {
 
 router.use('/add-product', (req, res, next)=> {
     // res.send('<form action="/product" method="POST"><input type="text" name="product"/><button type="submit">Add</button></form>');
-    res.sendFile(path.join(__dirname, '../', 'views', 'add-product.html'));
+    res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
 });
 router.use('/product', (req, res, next)=>{
     console.log(req.body);
--- routes/shop.js
+++ routes/shop.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const path = require('path');
-
+const rootDir = require('../utils/path');
 const router = express.Router();
 
 //Make sure exact match with / [GET]
 router.get('/', (req, res, next) => {
-    res.sendFile(path.join(__dirname, '../', 'views', 'shop.html'));
+    res.sendFile(path.join(rootDir, 'views', 'shop.html'));
 });
 
 module.exports = router;

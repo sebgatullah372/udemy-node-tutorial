d504ca4049004df588e4c7c7be824bae893c1a42
Md Sebgatullah Mahbub <smahbub@vantage.com>
2025-04-18 21:39:21 +0600

    cleaned up code keeping only ejs template

--- routes/admin.js
+++ routes/admin.js
@@ -4,10 +4,6 @@ const rootDir = require('../utils/path');
 const router = express.Router();
 const productsController = require('../controllers/products');
 
-router.use('/users', (req, res, next) => { 
-    res.sendFile(path.join(rootDir, 'views', 'users.html'));
-});
-
 router.get('/add-product', productsController.create);
 router.post('/add-product', productsController.store);
 
